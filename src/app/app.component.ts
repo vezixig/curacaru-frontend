0278e89f0f789b11f1bd0b99bda1df24fae3a29b
vezixig <vezixig@googlemail.com>
2023-11-20 18:13:43 +0100

    Added icons to sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,18 @@ import { RouterOutlet } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { LoginComponent } from './auth/login/login.component';
 import { TopbarComponent } from './dashboard/topbar/topbar.component';
+import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent, TopbarComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    LoginComponent,
+    TopbarComponent,
+    SidebarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/dashboard/sidebar/sidebar.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCalendar, faClipboard } from '@fortawesome/free-regular-svg-icons';
+import {
+  faPersonCane,
+  faUserClock,
+  faUsersGear,
+} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'cura-sidebar',
+  standalone: true,
+  templateUrl: './sidebar.component.html',
+  imports: [CommonModule, FontAwesomeModule],
+})
+export class SidebarComponent {
+  faUsersGear = faUsersGear;
+  faPersonCane = faPersonCane;
+  faCalendar = faCalendar;
+  faUserClock = faUserClock;
+  faClipboard = faClipboard;
+}
