85b966538c9131aee370d1f5836de3bf6714e516
vezixig <vezixig@googlemail.com>
2023-12-06 18:28:18 +0100

    Changed icons and logo

--- src/app/dashboard/topbar/topbar.component.ts
+++ src/app/dashboard/topbar/topbar.component.ts
@@ -3,14 +3,18 @@ import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { environment } from '../../../environments/environment';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'cura-topbar',
   standalone: true,
   templateUrl: './topbar.component.html',
-  imports: [CommonModule, NgbDropdownModule],
+  imports: [CommonModule, NgbDropdownModule, FontAwesomeModule],
 })
 export class TopbarComponent {
+  faUser = faUser;
+
   constructor(private auth: AuthService) {}
 
   handleLogout(): void {
