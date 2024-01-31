162ed9b6e597186386ae9928edca3d2c01b34f2f
vezixig <vezixig@googlemail.com>
2024-01-31 20:00:00 +0100

    Lazy loading for all routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,55 +1,45 @@
 import { Routes } from '@angular/router';
-import { EmployeeEditorComponent } from '@curacaru/dashboard/employees/employee-editor/employee-editor.component';
-import { EmployeeListComponent } from '@curacaru/dashboard/employees/employee-list/employee-list.component';
-import { CustomerListComponent } from '@curacaru/dashboard/customers/customer-list/customer-list.component';
-import { CustomerEditorComponent } from '@curacaru/dashboard/customers/customer-editor/customer-editor.component';
-import { AppointmentsListComponent } from '@curacaru/dashboard/appointments/appointments-list/appointments-list.component';
-import { AppointmentsEditorComponent } from '@curacaru/dashboard/appointments/appointments-editor/appointments-editor.component';
-import { InsurancesListComponent } from '@curacaru/dashboard/insurances/insurances-list/insurances-list.component';
-import { InsurancesEditorComponent } from '@curacaru/dashboard/insurances/insurances-editor/insurances-editor.component';
-import { CompanyComponent } from '@curacaru/dashboard/company/company.component';
-import { DocumentsComponent } from './dashboard/documents/documents.component';
 
 export const routes: Routes = [
   {
     path: 'dashboard/appointments',
     children: [
       { path: '', redirectTo: 'list', pathMatch: 'full' },
-      { path: 'list', component: AppointmentsListComponent },
-      { path: 'new', component: AppointmentsEditorComponent },
-      { path: ':id', component: AppointmentsEditorComponent },
+      { path: 'list', loadComponent: () => import('@curacaru/dashboard/appointments/appointments-list/appointments-list.component').then((o) => o.AppointmentsListComponent) },
+      { path: 'new', loadComponent: () => import('@curacaru/dashboard/appointments/appointments-editor/appointments-editor.component').then((o) => o.AppointmentsEditorComponent) },
+      { path: ':id', loadComponent: () => import('@curacaru/dashboard/appointments/appointments-editor/appointments-editor.component').then((o) => o.AppointmentsEditorComponent) },
     ],
   },
-  { path: 'dashboard/company', component: CompanyComponent, pathMatch: 'full' },
+  { path: 'dashboard/company', loadComponent: () => import('@curacaru/dashboard/company/company.component').then((o) => o.CompanyComponent), pathMatch: 'full' },
   {
     path: 'dashboard/customers',
     children: [
       { path: '', redirectTo: 'list', pathMatch: 'full' },
-      { path: 'list', component: CustomerListComponent },
-      { path: 'new', component: CustomerEditorComponent },
-      { path: ':id', component: CustomerEditorComponent },
+      { path: 'list', loadComponent: () => import('@curacaru/dashboard/customers/customer-list/customer-list.component').then((o) => o.CustomerListComponent) },
+      { path: 'new', loadComponent: () => import('@curacaru/dashboard/customers/customer-editor/customer-editor.component').then((o) => o.CustomerEditorComponent) },
+      { path: ':id', loadComponent: () => import('@curacaru/dashboard/customers/customer-editor/customer-editor.component').then((o) => o.CustomerEditorComponent) },
     ],
   },
   {
     path: 'dashboard/documents',
-    component: DocumentsComponent,
+    loadComponent: () => import('@curacaru/dashboard/documents/documents.component').then((o) => o.DocumentsComponent),
   },
   {
     path: 'dashboard/employees',
     children: [
       { path: '', redirectTo: 'list', pathMatch: 'full' },
-      { path: 'list', component: EmployeeListComponent },
-      { path: 'new', component: EmployeeEditorComponent },
-      { path: ':id', component: EmployeeEditorComponent },
+      { path: 'list', loadComponent: () => import('@curacaru/dashboard/employees/employee-list/employee-list.component').then((o) => o.EmployeeListComponent) },
+      { path: 'new', loadComponent: () => import('@curacaru/dashboard/employees/employee-editor/employee-editor.component').then((o) => o.EmployeeEditorComponent) },
+      { path: ':id', loadComponent: () => import('@curacaru/dashboard/employees/employee-editor/employee-editor.component').then((o) => o.EmployeeEditorComponent) },
     ],
   },
   {
     path: 'dashboard/insurances',
     children: [
       { path: '', redirectTo: 'list', pathMatch: 'full' },
-      { path: 'list', component: InsurancesListComponent },
-      { path: 'new', component: InsurancesEditorComponent },
-      { path: ':id', component: InsurancesEditorComponent },
+      { path: 'list', loadComponent: () => import('@curacaru/dashboard/insurances/insurances-list/insurances-list.component').then((o) => o.InsurancesListComponent) },
+      { path: 'new', loadComponent: () => import('@curacaru/dashboard/insurances/insurances-editor/insurances-editor.component').then((o) => o.InsurancesEditorComponent) },
+      { path: ':id', loadComponent: () => import('@curacaru/dashboard/insurances/insurances-editor/insurances-editor.component').then((o) => o.InsurancesEditorComponent) },
     ],
   },
   { path: 'dashboard', redirectTo: 'dashboard/appointments', pathMatch: 'full' },
